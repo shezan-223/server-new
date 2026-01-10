849d894b1634b03af9044ba29725816f48e57088
shezan <shezanahmedsourav223@gmail.com>
2026-01-10 15:24:58 +0600

    server is uploaded

--- index.js
+++ index.js
@@ -14,3 +14,56 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+// sourav
+// Rddi0hToDvrpCcQ4
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://sourav:Rddi0hToDvrpCcQ4@mongopractice.2tbsahv.mongodb.net/?appName=MongoPractice";
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+const db =client.db("realstate")
+const realstatecollection =db.collection("realstatecollection")
+
+
+app.get ("/",(req,res)=>{
+
+res.send("Real state server is running ")
+})
+
+
+// Add properties
+app.post ("/properties",async(req,res)=>{
+  const property =req.body
+  const result =await realstatecollection.insertOne(property)
+  res.send (result)
+})
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
