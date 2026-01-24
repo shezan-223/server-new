f9c96fab3b21bb67c6fe11c9e6fdbe02ab80b281
shezan <shezanahmedsourav223@gmail.com>
2026-01-24 17:28:37 +0600

    client url added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4000
 app.use(cors({
   origin: [
     "http://localhost:5173", 
-    "https://your-frontend-domain.vercel.app" 
+    process.env.CLIENT_URL
   ],
   credentials: true
 }));
